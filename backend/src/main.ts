312243d07311641cc463b0317eebe32b2ff3719f
lollipop99 <supports@accounts.com>
2025-11-19 15:48:02 +0100

    Fix CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: true,
+    origin: [
+      'https://nextradex.netlify.app',
+      'http://localhost:3000'
+    ],
+    credentials: true,
   });
+
   await app.listen(5000);
 }
+
 bootstrap();
