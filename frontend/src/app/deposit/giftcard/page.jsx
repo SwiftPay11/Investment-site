87f596947ad44312a0a414c87826dd622fe89b81
lollipop99 <supports@accounts.com>
2025-11-27 22:47:40 +0100

    update giftcard

--- frontend/src/app/deposit/giftcard/page.jsx
+++ frontend/src/app/deposit/giftcard/page.jsx
@@ -84,7 +84,7 @@ export default function DepositGiftcardPage() {
       formData.append("file", file);
 
       const res = await fetch(
-        "http://localhost:5000/wallet/deposit-giftcard",
+        "https://investment-site-x6tr.onrender.com/wallet/deposit-giftcard",
         {
           method: "POST",
           body: formData,
